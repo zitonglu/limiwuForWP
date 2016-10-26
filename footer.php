6bdd51a67d74a496d39f3ff1ee652ccc7763544e
zitonglu <910109610@qq.com>
2016-10-26 15:57:57 +0800

    footer style

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
-	<p class="container">Copyright&nbsp;©&nbsp;<a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>-<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?>-<?php bloginfo('description'); ?></a>&nbsp;&nbsp;<?php echo get_option( 'zh_cn_l10n_icp_num' ); ?>&nbsp;
+	<p class="container text-center">Copyright&nbsp;©&nbsp;<a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>-<?php bloginfo('description'); ?>"><?php bloginfo('name'); ?>-<?php bloginfo('description'); ?></a>&nbsp;&nbsp;<?php echo get_option( 'zh_cn_l10n_icp_num' ); ?>&nbsp;
 	<!-- 站长统计代码 -->
 	<?php echo get_option('limiwu_statscode'); ?>
-	<b>Powered By <a href="http://www.wordpress.org/" target="_blank" rel="nofollow">Wordpress</a>.Theme by <a href="http://www.paipk.com" target="_blank">Paipk.com</a></b></p>
+	&nbsp;Powered By <a href="http://www.wordpress.org/" target="_blank" rel="nofollow">Wordpress</a>.Theme by <a href="http://www.paipk.com" target="_blank">Paipk.com</a></p>
 </div>
 <script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
